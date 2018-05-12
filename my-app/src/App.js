58a59ac1c2961a298b9202990cc9fb9e28183363
Danny Giusa <danny.giusa@gmail.com>
2018-05-12 13:55:46 +0200

    Simple Webpage with Button to show toggle states

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,21 +1,63 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import MyComponent from './MyComponent';
 
 class App extends Component {
+  
+  constructor(props) {
+    super(props);
+    this.state = {
+      isOn: true
+    };
+  }
+  
+  toggle = () => {
+    this.setState({isOn: !this.state.isOn})
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
+        <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
+          <h2>Welcome to React</h2>
+          <MyComponent name="Aktuelle Webtechnologien: Frameworks und Tools (SoSe18)" />
+        </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <button onClick={this.toggle}>
+          {this.state.isOn ? 'Button State: On' : 'Button State: Off'}
+        </button>
       </div>
     );
   }
 }
 
 export default App;
+
+// Simple page without any controll elements
+// import React, { Component } from 'react';
+// import logo from './logo.svg';
+// import './App.css';
+// import MyComponent from './MyComponent';
+
+// class App extends Component {
+//   render() {
+//     return (
+//       <div className="App">
+//         <header className="App-header">
+//           <img src={logo} className="App-logo" alt="logo" />
+//           <h1 className="App-title">Welcome to React</h1>
+//           <MyComponent name="Aktuelle Webtechnologien: Frameworks und Tools (SoSe18)" />
+//         </header>
+//         <p className="App-intro">
+//           To get started, edit <code>src/App.js</code> and save to reload.
+//         </p>
+//       </div>
+//     );
+//   }
+// }
+
+// export default App;
---
+++ my-app/src/MyComponent.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+const MyComponent = (props) => (
+    <div>Hello {props.name}!</div>
+);
+
+// class MyComponent extends Component {
+//   render() {
+//     return (
+//        <div>Hello World! from MyComponent :)</div>
+//     )
+//   }
+// };
+
+export default MyComponent;
