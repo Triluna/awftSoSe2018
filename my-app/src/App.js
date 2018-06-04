826270374089a40a13e6faa2fb550f579d9efcd9
unknown <triluna@gmx.de>
2018-06-04 11:00:01 +0200

    nix

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,8 +2,11 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import MyComponent from './MyComponent';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 class App extends Component {
+
+
   
   constructor(props) {
     super(props);
@@ -18,20 +21,37 @@ class App extends Component {
 
   render() {
     return (
+
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
           <MyComponent name="Aktuelle Webtechnologien: Frameworks und Tools (SoSe18)" />
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <button onClick={this.toggle}>
+          <ul className="list-group">
+              <li className="list-group-item">
+                  <div>Post1</div>
+                  <button onClick={this.editButton}> editieren</button>
+              </li>
+              <li className="list-group-item">
+                  <div>Post2</div>
+                  <button>editieren</button>
+              </li>
+              <li className="list-group-item">
+                  <div>Post3</div>
+                  <button>editieren</button>
+              </li>
+          </ul>
+
+          <button onClick={this.toggle}>
           {this.state.isOn ? 'Button State: On' : 'Button State: Off'}
         </button>
       </div>
     );
-  }
+                }
+
+    editButton() {
+
+    }
 }
  export default App;
